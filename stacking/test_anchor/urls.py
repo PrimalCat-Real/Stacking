2a22a64547d38df882b102f35c05a262a7728064
PrimalCat-Real <romanenko.denis2003@gmail.com>
2022-03-23 18:18:48 +0200

    firth migrate in django

--- stacking/test_anchor/urls.py
+++ stacking/test_anchor/urls.py
@@ -1,8 +1,15 @@
+from xml.dom.minidom import Document
 from django.urls import path
+from django.conf.urls.static import static
 from .views import *
+from django.conf import settings
 
 
 urlpatterns = [
     path('', anchor),
     path('test/', test),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- stacking/test_anchor/views.py
+++ stacking/test_anchor/views.py
@@ -7,6 +7,7 @@ def index(request):
     print(dir(request))
     return HttpResponse("Hello World")
 
+
 def anchor(request):
     print(dir(request))
     return HttpResponse("Hello World")
